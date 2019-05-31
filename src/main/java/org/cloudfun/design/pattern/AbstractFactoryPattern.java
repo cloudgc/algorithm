49aaf077760c6192c40d7e40d970e8b8c20c6cc9
sunzhongwen <sunzhongwen@fullgoalos.com.cn>
2019-05-31 18:59:55 +0800

    add factory and abstract factory design pattern

---
+++ src/main/java/org/cloudfun/design/pattern/AbstractFactoryPattern.java
@@ -0,0 +1,129 @@
+package org.cloudfun.design.pattern;
+
+/**
+ * Copyright @ 2019/5/31
+ *
+ * @author cloudgc
+ * @since 0.0.1
+ *
+ * 创建一系列相互关联的类的接口， 但是不用指定实体类
+ *
+ */
+public class AbstractFactoryPattern {
+
+    interface CarCreate{
+
+        void createWheel();
+
+        void createEngine();
+
+    }
+
+    static class BMWCarCreate implements CarCreate{
+
+        @Override
+        public void createWheel() {
+            System.out.println("create bmw wheel .....");
+        }
+
+        @Override
+        public void createEngine() {
+            System.out.println("create bmw engine .....");
+
+        }
+    }
+
+
+    static class AudiCarCreate implements CarCreate{
+
+        @Override
+        public void createWheel() {
+            System.out.println("create audi wheel .....");
+        }
+
+        @Override
+        public void createEngine() {
+
+            System.out.println("create audi engine .....");
+        }
+    }
+
+
+    interface CarSell{
+
+        String getSellName();
+
+    }
+
+    static class BMWCarSell implements CarSell{
+
+        @Override
+        public String getSellName() {
+            return "I am BMW Sell : Bob";
+        }
+    }
+
+    static class AudiCarSell implements CarSell{
+
+        @Override
+        public String getSellName() {
+            return "I am Audi Sell : Jack";
+
+        }
+    }
+
+
+
+    interface  CarFacotry{
+
+        CarCreate getCarCreate(String name);
+
+        CarSell getCarSeil(String name);
+    }
+
+
+    static class  CarFactoryImp implements CarFacotry{
+
+        @Override
+        public CarCreate getCarCreate(String name) {
+
+            if("BMW".equals(name)){
+                return new BMWCarCreate();
+            }else  if("audi".equals(name)){
+                return new AudiCarCreate();
+            }
+
+            return null;
+        }
+
+        @Override
+        public CarSell getCarSeil(String name) {
+
+            if("BMW".equals(name)){
+                return new BMWCarSell();
+            }else if ("audi".equals(name)){
+                return new AudiCarSell();
+            }
+            return null;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        CarFactoryImp carFactoryImp = new CarFactoryImp();
+        CarCreate bmw = carFactoryImp.getCarCreate("BMW");
+
+        bmw.createWheel();
+        bmw.createEngine();
+
+        CarSell bmw1 = carFactoryImp.getCarSeil("BMW");
+        System.out.println(bmw1.getSellName());
+
+    }
+
+
+
+
+
+}
---
+++ src/main/java/org/cloudfun/design/pattern/FactoryPattern.java
@@ -0,0 +1,66 @@
+package org.cloudfun.design.pattern;
+
+/**
+ * Copyright @ 2019/5/31
+ *
+ * @author cloudgc
+ * @since 0.0.1
+ * <p>
+ * 定义一个创建对象的接口，让其子类自己决定实例化哪一个工厂类，工厂模式使其创建过程延迟到子类进行。
+ *
+ *   interface>>car
+ *    --------------
+ *    ↑      ↑
+ * bmwcar audicar
+ *
+ */
+public class FactoryPattern {
+
+    interface Car {
+        void drive();
+    }
+
+    static class BMWCar implements Car {
+
+        @Override
+        public void drive() {
+            System.out.println("drive faster");
+        }
+    }
+
+    static class AudiCar implements Car {
+
+        @Override
+        public void drive() {
+            System.out.println("drive slow");
+        }
+    }
+
+
+    static class CarFactory {
+
+        public static Car createCar(String name) {
+            if ("BMW".equals(name)) {
+                return new BMWCar();
+            } else if ("audi".equals(name)) {
+                return new AudiCar();
+            }
+            return null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Car bmw = CarFactory.createCar("BMW");
+
+        bmw.drive();
+
+        Car audi = CarFactory.createCar("audi");
+
+        audi.drive();
+
+
+    }
+
+}
