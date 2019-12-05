a0e6793eef2d4d660f87ea6d015c0ab4680100c4
cloudgc <cloudgc@126.com>
2019-12-05 19:10:12 +0800

    归并排序

--- src/main/java/org/cloudfun/algorithm/sort/MergeSort.java
+++ src/main/java/org/cloudfun/algorithm/sort/MergeSort.java
@@ -1,5 +1,7 @@
 package org.cloudfun.algorithm.sort;
 
+import java.util.Arrays;
+
 /**
  * @author cloudgc
  * @apiNote
@@ -10,13 +12,60 @@ package org.cloudfun.algorithm.sort;
  */
 public class MergeSort {
 
-    public int[] mergeSort(int[] array){
-        return null;
+    public int[] mergeSort(int[] array) {
+
+        if (array.length == 1) {
+            return array;
+        }
+
+        int i = array.length / 2;
+
+        int[] left = Arrays.copyOfRange(array, 0, i);
+        int[] right = Arrays.copyOfRange(array, i, array.length);
+
+        int[] leftMerge = mergeSort(left);
+        int[] rightMerge = mergeSort(right);
+        return mergeData(leftMerge, rightMerge);
+    }
+
+    private int[] mergeData(int[] leftMerge, int[] rightMerge) {
+        int[] finalData = new int[leftMerge.length + rightMerge.length];
+
+        int indexLeft = 0;
+        int indexRight = 0;
+        do {
+            if (indexLeft >= leftMerge.length) {
+                finalData[indexLeft + indexRight] = rightMerge[indexRight];
+                indexRight++;
+                continue;
+            }
+            if (indexRight >= rightMerge.length) {
+                finalData[indexLeft + indexRight] = leftMerge[indexLeft];
+                indexLeft++;
+                continue;
+            }
+
+            int left = leftMerge[indexLeft];
+            int right = rightMerge[indexRight];
+            if (left > right) {
+                finalData[indexLeft + indexRight] = right;
+                indexRight++;
+            } else {
+                finalData[indexLeft + indexRight] = left;
+                indexLeft++;
+            }
+        } while (indexLeft < leftMerge.length || indexRight < rightMerge.length);
+
+        return finalData;
+
     }
 
 
     public static void main(String[] args) {
-
-
+        int[] data = {1, 8, 10, 60, 5, 6, 7, 4, 3, 9, 0};
+        MergeSort mergeSort = new MergeSort();
+        int[] ints = mergeSort.mergeSort(data);
+        System.out.println(Arrays.toString(ints));
     }
+
 }
