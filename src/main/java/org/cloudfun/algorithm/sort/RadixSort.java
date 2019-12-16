551259aba49f2f9e0b3dc5db301ab02f00a3afe0
cloudgc <cloudgc@126.com>
2019-12-16 12:14:51 -0600

    start radix sort

--- src/main/java/org/cloudfun/algorithm/sort/RadixSort.java
+++ src/main/java/org/cloudfun/algorithm/sort/RadixSort.java
@@ -6,4 +6,26 @@ package org.cloudfun.algorithm.sort;
  * @date 2019/12/12
  */
 public class RadixSort {
+
+    public Integer[] radixSort(Integer[] arrays) {
+
+
+        return null;
+    }
+
+    public Integer[] generateData(int length) {
+
+        if (length > 32) {
+            throw new IllegalArgumentException("length is to big");
+        }
+
+        Integer[] data = new Integer[length];
+        for (int i = 0; i < length; i++) {
+            double v = Math.round(Math.random() * (10 ^ length));
+            data[i] = Double.valueOf(v).intValue();
+        }
+        return data;
+    }
+
+
 }
