03603f940cb20bee2cfeee5eb8fe7826998e7f67
cloudgc <cloudgc@126.com>
2019-12-17 18:22:28 -0600

    finish radix sort

--- src/main/java/org/cloudfun/algorithm/sort/RadixSort.java
+++ src/main/java/org/cloudfun/algorithm/sort/RadixSort.java
@@ -1,5 +1,9 @@
 package org.cloudfun.algorithm.sort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author cloudgc
  * @apiNote
@@ -7,25 +11,61 @@ package org.cloudfun.algorithm.sort;
  */
 public class RadixSort {
 
-    public Integer[] radixSort(Integer[] arrays) {
+    BucketSort bucketSort = new BucketSort();
 
 
-        return null;
+    public Integer[] radixSort(Integer[] arrays, int start) {
+
+
+        if (start >= 10 || arrays.length <= 1) {
+            return arrays;
+        }
+
+        int bucketLength = 1;
+
+        List<List<Integer>> buckets = new ArrayList<>();
+        for (int i = 0; i <= 10; i += bucketLength) {
+            buckets.add(new ArrayList<>());
+        }
+
+        for (Integer ele : arrays) {
+            int bucketIndex = Integer.parseInt(Integer.toString(ele).substring(start, start + 1));
+            buckets.get(bucketIndex).add(ele);
+        }
+
+        List<Integer> data = new ArrayList<>();
+        for (List<Integer> bucket : buckets) {
+            Integer[] buks = new Integer[bucket.size()];
+            Integer[] integers = radixSort(bucket.toArray(buks), start + 1);
+            data.addAll(Arrays.asList(integers));
+        }
+
+        Integer[] dataList = new Integer[data.size()];
+        return data.toArray(dataList);
     }
 
     public Integer[] generateData(int length) {
 
-        if (length > 32) {
+        if (length > (100000000)) {
             throw new IllegalArgumentException("length is to big");
         }
 
         Integer[] data = new Integer[length];
         for (int i = 0; i < length; i++) {
-            double v = Math.round(Math.random() * (10 ^ length));
+            double v = Math.round(Math.random() * (1000000000));
             data[i] = Double.valueOf(v).intValue();
         }
         return data;
     }
 
 
+    public static void main(String[] args) {
+        RadixSort radixSort = new RadixSort();
+        Integer[] integers = radixSort.generateData(10);
+        System.out.println(Arrays.toString(integers));
+
+        Integer[] integers1 = radixSort.radixSort(integers, 0);
+        System.out.println(Arrays.toString(integers1));
+
+    }
 }
