810e726d453cfaf0575f9e9c95cfbfdcb11bf6de
cloudgc <cloudgc@126.com>
2019-12-31 16:26:16 +0800

    traversal binary tree

--- src/main/java/org/cloudfun/algorithm/binarytree/BinaryTree.java
+++ src/main/java/org/cloudfun/algorithm/binarytree/BinaryTree.java
@@ -1,5 +1,8 @@
 package org.cloudfun.algorithm.binarytree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author cloudgc
  * @apiNote
@@ -47,25 +50,117 @@ public class BinaryTree {
 
         TreeNode treeNode = buildFullTree(data);
 
-
+        System.out.println();
         printPreOrderTree(treeNode);
 
+        System.out.println();
         printInOrderTree(treeNode);
 
+        System.out.println();
         printPostOrderTree(treeNode);
 
+        System.out.println();
+        printByLevel(treeNode);
 
     }
 
+    /**
+     * 后序遍历  右叶子节点 -> 本节点 -> 左叶子节点
+     *
+     * @param treeNode data
+     */
     private static void printPostOrderTree(TreeNode treeNode) {
+        if (treeNode == null) {
+            return;
+        }
+
+        printPostOrderTree(treeNode.getRightLeaf());
+
+        printPostOrderTree(treeNode.getLeftLeaf());
+
+        System.out.print(treeNode.getElement() + ",");
+
     }
 
+    /**
+     * 中序遍历  左叶子节点 -> 本节点 -> 右叶子节点
+     *
+     * @param treeNode data
+     */
     private static void printInOrderTree(TreeNode treeNode) {
+        if (treeNode == null) {
+            return;
+        }
 
+        printInOrderTree(treeNode.getLeftLeaf());
+
+        System.out.print(treeNode.getElement() + ",");
+
+        printInOrderTree(treeNode.getRightLeaf());
     }
 
+    /**
+     * 前序遍历  本节点 -> 左叶子节点 -> 右叶子节点
+     *
+     * @param treeNode data
+     */
     private static void printPreOrderTree(TreeNode treeNode) {
 
+        if (treeNode == null) {
+            return;
+        }
+
+        System.out.print(treeNode.getElement() + ",");
+
+        printPreOrderTree(treeNode.getLeftLeaf());
+
+        printPreOrderTree(treeNode.getRightLeaf());
+
+    }
+
+
+    /**
+     * 按层遍历
+     *
+     * @param treeNode data
+     */
+    public static void printByLevel(TreeNode treeNode) {
+
+        Queue<TreeNode> rootQueue = new LinkedList<>();
+
+        Queue<TreeNode> mirrorQueue = new LinkedList<>();
+
+        rootQueue.offer(treeNode);
+
+        printLevelNode(rootQueue, mirrorQueue);
+
+
+    }
+
+    private static void printLevelNode(Queue<TreeNode> rootQueue, Queue<TreeNode> mirrorQueue) {
+
+        if (rootQueue.isEmpty()) {
+            return;
+        }
+        printLevel(mirrorQueue, rootQueue);
+        printLevel(rootQueue, mirrorQueue);
+        printLevelNode(rootQueue, mirrorQueue);
+    }
+
+    private static void printLevel(Queue<TreeNode> rootQueue, Queue<TreeNode> mirrorQueue) {
+        while (!mirrorQueue.isEmpty()) {
+            TreeNode nextNode = mirrorQueue.poll();
+            System.out.print(nextNode.getElement()+" ");
+            if (nextNode.getLeftLeaf() != null) {
+                rootQueue.offer(nextNode.getLeftLeaf());
+            }
+
+            if (nextNode.getRightLeaf() != null) {
+                rootQueue.offer(nextNode.getRightLeaf());
+            }
+        }
+
+        System.out.println();
     }
 
 
