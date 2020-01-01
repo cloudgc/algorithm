82c12566e9274742ffa9d5d3f6fe733ec66f891e
cloudgc <cloudgc@126.com>
2020-01-01 14:58:34 -0600

    add Binary Tree Search

---
+++ src/main/java/org/cloudfun/algorithm/binarytree/BinaryTreeOperator.java
@@ -0,0 +1,16 @@
+package org.cloudfun.algorithm.binarytree;
+
+/**
+ * @author cloudgc
+ * @apiNote
+ * @date 2020/1/6
+ */
+public class BinaryTreeOperator {
+
+
+    public TreeNode findTreeNode(int data){
+        return null;
+    }
+
+
+}
