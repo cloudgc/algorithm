15cfb622e137d83664b1e2546270e967ed65a4d4
cloudgc <cloudgc@126.com>
2020-01-06 15:56:11 +0800

    full insert method

--- src/main/java/org/cloudfun/algorithm/binarytree/BinaryTreeOperator.java
+++ src/main/java/org/cloudfun/algorithm/binarytree/BinaryTreeOperator.java
@@ -8,7 +8,7 @@ package org.cloudfun.algorithm.binarytree;
 public class BinaryTreeOperator {
 
     public static void main(String[] args) {
-        int[] data = {0, 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15};
+        int[] data = {0, 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 16};
         TreeNode treeNode = BinaryTree.buildFullTree(data);
 
         // 中序遍历
@@ -18,6 +18,11 @@ public class BinaryTreeOperator {
         //查找节点
         TreeNode findNode = findTreeNode(treeNode, 10);
         System.out.println(findNode);
+
+        //插入节点
+        TreeNode insertTreeNode = insertTreeNode(treeNode, 15);
+        BinaryTree.printInOrderTree(insertTreeNode);
+        System.out.println();
 
 
     }
@@ -43,8 +48,30 @@ public class BinaryTreeOperator {
         return find;
     }
 
-    public static TreeNode insertTreeNode(int data) {
-        return null;
+    public static TreeNode insertTreeNode(TreeNode treeNode, int data) {
+        if (treeNode == null) {
+            return new TreeNode(data);
+        }
+
+        TreeNode tmp = treeNode;
+        while (tmp != null) {
+            if (tmp.getElement() > data) {
+                if (tmp.getLeftLeaf() == null) {
+                    tmp.setLeftLeaf(new TreeNode(data));
+                }
+                tmp = tmp.getLeftLeaf();
+            } else if (tmp.getElement() < data) {
+                if (tmp.getRightLeaf() == null) {
+                    tmp.setRightLeaf(new TreeNode(data));
+                }
+                tmp = tmp.getRightLeaf();
+            } else {
+                return treeNode;
+            }
+        }
+
+
+        return treeNode;
     }
 
     public static TreeNode removeTreeNode(int data) {
