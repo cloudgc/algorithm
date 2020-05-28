87d469ac7e55ac653ce04623b643e160f946f3b2
cloudgc <cloudgc@126.com>
2020-05-28 23:03:19 +0800

    heap data

--- src/main/java/org/cloudfun/algorithm/binarytree/Heap.java
+++ src/main/java/org/cloudfun/algorithm/binarytree/Heap.java
@@ -29,51 +29,73 @@ public class Heap {
     }
 
     public Heap(int capacity) {
-        innerArray = new int[capacity + 1];
+        innerArray = new int[capacity];
         maxLength = capacity;
-        count = 0;
+        count = -1;
     }
 
+    /**
+     * @param ele add element
+     */
     public void addElement(int ele) {
-        if (count >= maxLength) {
+        if (count + 1 >= maxLength) {
             return;
         }
         ++count;
         innerArray[count] = ele;
 
         int i = count;
-        while (i / 2 > 0 && innerArray[i] > innerArray[i / 2]) {
-            swap(innerArray, i, i / 2);
-            i = i / 2;
+        while ((i + 1) / 2 > 0 && innerArray[i] > innerArray[(i - 1) / 2]) {
+            swap(innerArray, i, (i - 1) / 2);
+            i = (i - 1) / 2;
         }
     }
 
+    /**
+     * swap one & other position
+     *
+     * @param innerArray array
+     * @param i          one
+     * @param j          other
+     */
     private void swap(int[] innerArray, int i, int j) {
         int tmp = innerArray[i];
         innerArray[i] = innerArray[j];
         innerArray[j] = tmp;
     }
 
+    /**
+     * remove top of heap element
+     * 交换到最后一个元素然后 建堆
+     */
     public void removeMaxElement() {
-        swap(innerArray, 1, count);
+        swap(innerArray, 0, count);
         innerArray[count] = 0;
         --count;
-        heapify(innerArray, count, 1);
+        heapify(innerArray, count, 0);
 
 
     }
 
-    private void heapify(int[] innerArray, int t, int i) {
+    /**
+     * 堆化 某个元素
+     *
+     * @param innerArray array
+     * @param count      array current count
+     * @param i          start position
+     */
+    private void heapify(int[] innerArray, int count, int i) {
         while (true) {
             int maxPos = i;
 
-            if (i * 2 <= t && innerArray[i] < innerArray[i * 2]) {
-                maxPos = i * 2;
+            if ((i + 1) * 2 - 1 <= count && innerArray[i] < innerArray[(i + 1) * 2 - 1]) {
+                maxPos = (i + 1) * 2 - 1;
             }
-            if (i * 2 + 1 < t && innerArray[i] < innerArray[i * 2 + 1]) {
-                maxPos = i * 2 + 1;
+            if ((i + 1) * 2 < count && innerArray[i] < innerArray[(i + 1) * 2]) {
+                maxPos = (i + 1) * 2;
             }
             if (maxPos == i) {
+
                 break;
             }
 
@@ -90,15 +112,30 @@ public class Heap {
     }
 
 
+    public Heap buildHeap(int[] array) {
+
+
+        return this;
+    }
+
+    public void sort() {
+
+    }
+
+
     public static void main(String[] args) {
         Heap heap = new Heap(5);
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i < 6; i++) {
             heap.addElement(i);
         }
 
         heap.print();
 
+
+        heap.removeMaxElement();
+
+        heap.print();
 
         heap.removeMaxElement();
 
