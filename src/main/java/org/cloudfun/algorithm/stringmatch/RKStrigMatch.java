ac6a5e61b84ebdb2ce59a235eea20cfcfa0dd469
cloudgc <cloudgc@126.com>
2021-11-24 00:14:08 +0800

    heap data

---
+++ src/main/java/org/cloudfun/algorithm/stringmatch/RKStrigMatch.java
@@ -0,0 +1,17 @@
+package org.cloudfun.algorithm.stringmatch;
+
+/**
+ *   Rabin-Karp
+ */
+public class RKStrigMatch {
+    
+    
+    private static int[] DICT_HASH = {26,26*26, 26*26*26};
+    
+    public static void main(String[] args) {
+    
+        System.out.println(DICT_HASH);
+        
+        
+    }
+}
